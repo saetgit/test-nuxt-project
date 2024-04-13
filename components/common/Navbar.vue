c36c86e18ff623dcf277748a3e06295c41dbeefe
saetgit <etemadian.sara.1998@gmail.com>
2024-04-13 18:42:17 +0330

    add baner component

--- components/common/Navbar.vue
+++ components/common/Navbar.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="flex items-center justify-between h-[110px] shadow-lg p-4">
-        <img src="@/assets/image/icon.png" class="w-[400px]" />
-        <ul class="flex gap-4 mx-[50px]">
-            <li v-for="(item, index) in menuItems" :key="index" :class="{ active: activeItem === index }"
-                @click="activeItem = index" class="px-4">{{ item }}</li>
-        </ul>
-        <div class="flex items-center justify-end  bg-gray-100 rounded-md px-4 ">
-            <img src="@/assets/svg/search.svg" alt="search"  />
-            <input class="border-none px-4 py-2 bg-transparent" type="text" />
+    <div class=" flex items-center h-[110px] shadow-lg p-4">
+        <div class="container mx-auto justify-between flex items-center">
+            <img src="@/assets/image/icon.png" alt="icon" class="w-[300px]" />
+            <ul class="flex gap-4 mx-[50px]">
+                <li v-for="(item, index) in menuItems" :key="index" :class="{ active: activeItem === index }"
+                    @click="activeItem = index" class="px-4">{{ item }}</li>
+            </ul>
+            <div class="flex items-center justify-end  bg-gray-100 rounded-md px-4 ">
+                <img src="@/assets/svg/search.svg" alt="search" />
+                <input class="border-none px-4 py-2 bg-transparent outline-none" type="text" />
+            </div>
         </div>
     </div>
 </template>
@@ -21,5 +23,9 @@ const activeItem = ref(0);
 <style scoped>
 .active {
     border-right: 2px solid #f29daf;
+}
+
+li {
+    cursor: pointer;
 }
 </style>
