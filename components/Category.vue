284c85f4b3baab6c9be64e77b3e31cf4c6df673c
saetgit <etemadian.sara.1998@gmail.com>
2024-04-13 19:25:09 +0330

    add json-server

---
+++ components/Category.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <div v-for="(category, index) in categories" :key="index" :style="{ backgroundColor: category.bgColor }">
+        <div>
+          <img :src="require(category.imageUrl)" alt="Category Image" />
+        </div>
+        <div>
+          <p>{{ category.title }}</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['categories']
+  };
+  </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="container mx-auto">
-        <Baner />
-        <Search/>
+      <Baner />
+      <Search />
+      <h3>What do you want done for you?</h3>
+      <Category :categories="categories" />
     </div>
-</template>
+  </template>
+  
+  <script>
+  export default {
+    async fetch() {
+      const response = await fetch('/data/db.json');
+      const data = await response.json();
+      return { categories: data.categories };
+    }
+  };
+  </script
